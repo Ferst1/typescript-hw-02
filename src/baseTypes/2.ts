6c4f465ec73a023c81cbb6be9f33dac02f6b3909
ElenaK <helenochka4@gmail.com>
2023-12-06 14:37:33 +0100

    updated

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,5 +1,7 @@
-let anything = -20;
-anything = 'Text';
+//*Який тип ви надаєте змінній anything в TypeScript, щоб зберегти її гнучкість?
+
+let anything: any = -20;
+anything = "Text";
 anything = {};
 
 export {};
